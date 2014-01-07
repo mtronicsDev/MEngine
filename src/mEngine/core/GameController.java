b48f9c4a7fe6ac6218c691efcb43ad251c536dfc
mtronicsDev <maxi.schmeller@live.de>
2014-01-07 18:02:05 +0100

    Surprise: Work on the base structure...

--- src/mEngine/core/GameController.java
+++ src/mEngine/core/GameController.java
@@ -1,8 +1,15 @@
 package mEngine.core;
 
+import mEngine.graphics.GraphicsController;
+
 public class GameController {
 
-    public static void runGame() {}
+    public static void runGame() {
+
+        GraphicsController.createDisplay(1280, 720, 60, "mEngine Test Run", false);
+        GameLoop.loop();
+
+    }
     public static void stopGame() {}
 
 }
---
+++ src/mEngine/interactive/gui/GUIContainer.java
@@ -0,0 +1,7 @@
+package mEngine.interactive.gui;
+
+public class GUIContainer {
+
+    
+
+}
