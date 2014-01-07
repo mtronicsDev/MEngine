296b0391533bebcfdcd4de736f808b9b527e9354
lukDev <luk4437@googlemail.com>
2014-01-07 17:58:41 +0100

    added some game objects

---
+++ src/mEngine/physics/Collider.java
@@ -0,0 +1,19 @@
+package mEngine.physics;
+
+import mEngine.gameObjects.GameObject;
+
+public class Collider {
+
+       public static boolean areAABBColliding(GameObject a, GameObject b) {
+
+           boolean colliding = false;
+
+
+
+           return colliding;
+
+       }
+
+
+
+}
