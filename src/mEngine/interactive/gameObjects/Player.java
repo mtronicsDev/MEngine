d6bc576b76377cad8e165262387de3ef6dc61f61
mtronicsDev <maxi.schmeller@live.de>
2014-01-12 00:45:27 +0100

    Player update??

--- src/mEngine/interactive/gameObjects/Player.java
+++ src/mEngine/interactive/gameObjects/Player.java
@@ -4,6 +4,6 @@ import org.lwjgl.util.vector.Vector3f;
 
 public class Player extends GameObjectRenderable {
 
-    public  Player(Vector3f pos, Vector3f rot, String modelFileName) { super(pos, rot, modelFileName);}
+    public Player(Vector3f pos, Vector3f rot, String modelFileName) { super(pos, rot, modelFileName); }
 
 }
