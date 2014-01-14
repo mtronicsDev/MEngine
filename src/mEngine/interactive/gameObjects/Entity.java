3d2df8dce25f0a6f193fb84bf471b3115b8bf6ca
lukDev <luk4437@googlemail.com>
2014-01-14 15:31:42 +0100

    added an entity class

---
+++ src/mEngine/interactive/gameObjects/Entity.java
@@ -0,0 +1,9 @@
+package mEngine.interactive.gameObjects;
+
+import org.lwjgl.util.vector.Vector3f;
+
+public class Entity extends GameObjectRenderable {
+
+    public Entity(Vector3f pos, Vector3f rot, String modelFileName) { super(pos, rot, modelFileName); }
+
+}
--- src/mEngine/interactive/gameObjects/Player.java
+++ src/mEngine/interactive/gameObjects/Player.java
@@ -2,7 +2,7 @@ package mEngine.interactive.gameObjects;
 
 import org.lwjgl.util.vector.Vector3f;
 
-public class Player extends GameObjectRenderable {
+public class Player extends Entity {
 
     public Player(Vector3f pos, Vector3f rot, String modelFileName) { super(pos, rot, modelFileName); }
 
