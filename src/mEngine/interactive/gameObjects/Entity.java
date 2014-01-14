13f6ded0a944a4edeec23aaba44c998228e82f65
mtronicsDev <maxi.schmeller@live.de>
2014-01-14 19:12:57 +0100

    Starting with uv work :)

--- src/mEngine/interactive/gameObjects/Entity.java
+++ src/mEngine/interactive/gameObjects/Entity.java
@@ -4,7 +4,7 @@ import org.lwjgl.util.vector.Vector3f;
 
 public class Entity extends GameObjectRenderable {
 
-    public Entity(Vector3f pos, Vector3f rot, String modelFileName) { super(pos, rot, modelFileName); }
+    public Entity(Vector3f pos, Vector3f rot, String modelFileName, String textureFileName) { super(pos, rot, modelFileName, textureFileName); }
 
     public void updateEntity() {
 
--- src/mEngine/interactive/gameObjects/Player.java
+++ src/mEngine/interactive/gameObjects/Player.java
@@ -4,7 +4,7 @@ import org.lwjgl.util.vector.Vector3f;
 
 public class Player extends Entity {
 
-    public Player(Vector3f pos, Vector3f rot, String modelFileName) { super(pos, rot, modelFileName); }
+    public Player(Vector3f pos, Vector3f rot, String modelFileName, String textureFileName) { super(pos, rot, modelFileName, textureFileName); }
 
     public void updatePlayer() {
 
