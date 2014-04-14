e6a3f646673a01438a8a73f8d356b6ed20cac679
mtronicsDev <maxi.schmeller@live.de>
2014-04-14 14:31:40 +0200

    Worked on terrain

--- src/mEngine/gameObjects/components/Terrain.java
+++ src/mEngine/gameObjects/components/Terrain.java
@@ -2,10 +2,20 @@ package mEngine.gameObjects.components;
 
 import mEngine.graphics.Renderer;
 import mEngine.graphics.renderable.Model;
+import org.lwjgl.util.vector.Vector3f;
 
 public class Terrain extends ComponentRenderable {
 
     protected Model model;
+    protected Vector3f size;
+    protected float[][] heightmap;
+
+    public Terrain(Vector3f size) {
+
+        this.size = size;
+        heightmap = new float[(int)size.x][(int)size.z];
+
+    }
 
     @Override
     public void addToRenderQueue() {
