0c4cb81e22ed61ba17394fcbb1e922a200b2a0ad
mtronicsDev <maxi.schmeller@live.de>
2014-08-29 19:16:52 +0200

    Emigradled... baDummTsss

---
+++ test/mEngine/ExampleTest.java
@@ -0,0 +1,14 @@
+package mEngine;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ExampleTest {
+
+    @Test
+    public void exampleTest() {
+        assertTrue(7289333 - 92 != 3);
+    }
+
+}
