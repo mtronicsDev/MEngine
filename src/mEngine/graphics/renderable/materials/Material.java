54704ae8c7884d65f4e6e04fdf53fc6a045580b3
mtronicsDev <maxi.schmeller@live.de>
2014-09-29 22:16:57 +0200

    #MNG-19 Added Material comments.

--- src/mEngine/graphics/renderable/materials/Material.java
+++ src/mEngine/graphics/renderable/materials/Material.java
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2014 mgamelabs
+ * To see our full license terms, please visit https://github.com/mgamelabs/mengine/blob/master/LICENSE.md
+ * All rights reserved.
+ */
+
 package mEngine.graphics.renderable.materials;
 
 import mEngine.graphics.renderable.textures.Texture;
@@ -12,10 +18,19 @@ public abstract class Material implements Serializable {
     public String textureName;
     public Color color = new Color(Color.white);
 
+    /**
+     * Returns the material texture
+     * @return The texture
+     */
     public Texture getTexture() {
         return texture;
     }
 
+    /**
+     * Sets the material texture
+     * @param texture The texture to use
+     * @return The material
+     */
     public Material setTexture(Texture texture) {
 
         this.texture = texture;
@@ -23,6 +38,11 @@ public abstract class Material implements Serializable {
 
     }
 
+    /**
+     * Sets only the texture name, the texture itself has to be set via setTextureFromName()
+     * @param textureName The name of the texture
+     * @return The material
+     */
     public Material setTextureName(String textureName) {
 
         this.textureName = textureName;
@@ -30,26 +50,49 @@ public abstract class Material implements Serializable {
 
     }
 
+    /**
+     * Sets the material texture from the previously set name
+     */
     public void setTextureFromName() {
         texture = TextureHelper.getTexture(textureName);
     }
 
+    /**
+     * Deletes the texture from the material
+     */
     public void deleteTexture() {
         texture = null;
     }
 
+    /**
+     * Determines if the material has a texture
+     * @return True if a texture <i>name</i> is present, false if not
+     */
     public boolean hasTexture() {
         return textureName != null;
     }
 
+    /**
+     * Determines if the material has a color
+     * @return True if the material has a color, false if not
+     */
     public boolean hasColor() {
         return color != null;
     }
 
+    /**
+     * Returns the material's color
+     * @return The material's color
+     */
     public Color getColor() {
         return color;
     }
 
+    /**
+     * Sets the material's color
+     * @param color The color to use
+     * @return The material
+     */
     public Material setColor(Color color) {
 
         this.color = new Color(color);
@@ -57,14 +100,20 @@ public abstract class Material implements Serializable {
 
     }
 
+    /**
+     * Removes the color from the material
+     */
     public void deleteColor() {
         setColor(null);
     }
 
+    /**
+     * Tells the Renderer to use <i>this</i> material.
+     * Automatically gets unbound when another material is bound
+     */
     public void bind() {
 
         if (texture != null) texture.getTexture().bind();
-
         else if (color != null) color.bind();
 
     }
